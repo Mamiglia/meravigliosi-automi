a2c73120152bfa918caab1c8e91caa2babea9cc1
Aivalf <aivalfantastic@gmail.com>
2022-03-30 14:54:44 +0200

    Aggiornamento evaluate

--- lib.js
+++ lib.js
@@ -1,29 +1,54 @@
-function evaluate(grafo, stringa){
-    nodes = Array(radice??);
-    return ricorsiveEvaluate(grafo, nodes, stringa); //nodes è l'insieme dei nodi "attivi", inizialmente la sola radice
+import * as vNG from "v-network-graph"
+import data from "./data"
+
+function checkSuccess(nodes){
+    for (node of nodes){
+        if(node è finale??){
+            return true;
+        }
+    }
+    return false;
 }
 
-//A ogni ricorsione viene consumato un carattere di stringa e
+function checkContains(myNode, nodes){
+    for (node of nodes){
+        if (node==myNode){
+            return true;
+        }
+    }
+    return false;
+}
+
+function checkTransition(myString, path){
+    //todo in base al path
+    return false;
+}
+
+function evaluate(automaton, myString){
+    //devo chiamare un costruttore per trasformare i dati in automa?
+    nodes = Array(automaton.nodes[0]);                   //assumendo che la radice sia il primo nodo di nodes
+    return ricorsiveEvaluate(automaton, nodes, myString); //nodes è l'insieme dei nodi "attivi", inizialmente la sola radice
+}
+
+//A ogni ricorsione viene consumato un carattere di myString e
 //la vecchia lista di nodi "attivi" viene completamente sostituita con i nodi raggiunti.
 //Se l'automa è a stati finiti la lista conterrà sempre un solo nodo.
-//Con un check si può usare quest'ultimo fatto anche per verificare che il grafo in input
+//Con un check si può usare quest'ultimo fatto anche per verificare che il automaton in input
 //rappresenti effettivamente un automa a stati finiti.
 //Ho provato a usare il commento multiriga ma ho fallito :)
-function ricorsiveEvaluate(grafo, nodes, stringa){
-    if (stringa==""){                               //caso base
-        if(nodes contiene uno stato finale??){
+function ricorsiveEvaluate(automaton, nodes, myString){
+    if (myString==""){                               //caso base
+        if(checkSucces(nodes)){            //se nodes contiene uno stato finale
             return true;
         }
         return false;
     }
     newNodes = Array();
-    for(node of nodes){
-        for(edge del nodr??){               //per ogni arco che parte da node
-            if(checkTransition(edge)){      //se la transizione è applicabile (checkTransition da definire)
-                newNodes.push(vicino??);    //aggiungo il nodo di arrivo di edge a newNodes
-            }
-        }
+    for(edge of automaton.edges){
+        if(checkContains(edge.source, nodes) and checkTransition(edge) and not checkContains(edge.target, newNodes)){
+            newNodes.push(edge.target);    //aggiungo il nodo di arrivo di edge a newNodes
+
     }
-    newStringa = stringa.slice(1);
-    return ricorsiveEvaluate(grafo, newNodes, newStringa);
+    newString = myString.slice(1);
+    return ricorsiveEvaluate(automaton, newNodes, newString);
 }
