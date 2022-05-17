764c997730731fa06a163625ac815d4f3bb78273
Matteo Migliarini <33762284+Mamiglia@users.noreply.github.com>
2022-05-17 18:41:33 +0000

    Rename file, new predefined const
    new const is needed in order to have the correct palette when displaying images

--- src/assets/predefined.ts
+++ src/assets/predefined.ts
@@ -84,3 +84,71 @@ export const networkGraphConfigs = defineConfigs({
         }
     }
 })
+
+export const palette = `
+:root {
+    /* colors: */
+      --white: #fbfaf5;
+      --white-ish: #ddd5d5;
+    
+      --black: #181818;
+      --black-ish: #252c2c;
+    
+      --indigo: #2c3e50;
+    
+      --rosso-sapienza: #822433; /* <3 */
+    
+      --vt-c-text-light-1: var(--vt-c-indigo);
+      --vt-c-text-light-2: rgba(60, 60, 60, 1);
+      --vt-c-text-dark-1: var(--vt-c-white);
+      --vt-c-text-dark-2: rgba(235, 235, 235, 1);
+    
+    
+    /* palette: */
+      --main: var(--rosso-sapienza) ;
+    
+      --complement: #248273;
+    
+      --fast-animation: 0.35s;
+      --normal-animation: 0.75s; 
+      background: var(--background);  
+    }
+    
+    /* semantic color variables for this project */
+    :root {
+      --call-to-action: var(--rosso-sapienza);
+      --secondary: var(--complement);
+      --primary-dark: var(--white-ish);
+      --primary-light: var(--white);
+    
+      --background: var(--primary-light);
+      --background-alternative: var(--primary-dark);
+    
+      
+      --color-grid: var(--primary-dark);
+      
+      --color-heading: var(--vt-c-text-light-1);
+      --color-text: var(--vt-c-text-light-1);
+      
+      --accent: var(--call-to-action);
+    }
+    
+    @media (prefers-color-scheme: dark) {
+      :root {
+        --call-to-action: var(--rosso-sapienza);
+        --secondary: var(--complement);
+        --primary-dark: var(--black);
+        --primary-light: var(--black-ish);
+      
+        --background: var(--primary-dark);
+        --background-alternative: var(--primary-light);
+      
+        --color-grid: var(--primary-light);
+      
+        --color-heading: var(--vt-c-text-dark-1);
+        --color-text: var(--vt-c-text-dark-2);
+      
+        --accent: var(--call-to-action);
+      } 
+    }
+`
