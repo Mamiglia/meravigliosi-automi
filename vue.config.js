84e9b543baf94cc30513193fa943ec473051c5c0
Matteo Migliarini <33762284+Mamiglia@users.noreply.github.com>
2022-05-23 20:59:13 +0000

    Web app

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,20 @@ module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
   ? '/meravigliosi-automi/'
-  : ''
+  : '',
+  pwa: {
+    name: 'Meravigliosi Automi',
+    themeColor: '#822433',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black'
+
+    // // configure the workbox plugin
+    // workboxPluginMode: 'InjectManifest',
+    // workboxOptions: {
+    //   // swSrc is required in InjectManifest mode.
+    //   swSrc: 'dev/sw.js',
+    //   // ...other Workbox options...
+    // }
+  }
 })
